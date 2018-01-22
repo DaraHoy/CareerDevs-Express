5f850624633ba2855ac3d06c18fb57999faa7d3d
Dara Hoy <darahoy@gmail.com>
2018-01-22 19:37:45 +0000

    Complete level 1 requirements

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var app = express();
+var name = 'Dara';
+var d = new Date;
 
+app.get('/', function(req, res){
+    res.send('hello world');
+});
+app.get('/name', function(req, res){
+    res.send(name);
+});
+
+app.get('/redirect', function(req, res){
+   res.redirect(301, '/surprise'); 
+});
+
+app.get('/date', function(req, res) {
+    res.send(d);
+})
 
 
 
